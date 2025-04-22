c3cfd2d609f51cead3c85d690797a05c599de26c
William Jiang <w.jiang156@gmail.com>
2025-04-22 12:59:37 -0500

    done with training loop

--- dataloader.py
+++ dataloader.py
@@ -1,29 +1,31 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 from torch.utils.data import DataLoader
 
+
 class KDDCupData:
-    def __init__(self, data_dir, mode):
+    def __init__(self, key, data_dir, mode):
         """Loading the data for train and test."""
         data = jnp.load(data_dir, allow_pickle=True)
 
-        labels = data["kdd"][:,-1]
-        features = data["kdd"][:,:-1]
-        #In this case, "attack" has been treated as normal data as is mentioned in the paper
-        normal_data = features[labels==0] 
-        normal_labels = labels[labels==0]
+        labels = data["kdd"][:, -1]
+        features = data["kdd"][:, :-1]
+        # non-anomalous data is the data that is not labeled "normal."
+        normal_data = features[labels == 0]
+        normal_labels = labels[labels == 0]
 
-        n_train = int(normal_data.shape[0]*0.5)
-        ixs = jax.random.permutation(normal_data.shape[0])
+        n_train = int(normal_data.shape[0] * 0.5)
+        ixs = jax.random.permutation(key, normal_data.shape[0])
         normal_data_test = normal_data[ixs[n_train:]]
         normal_labels_test = normal_labels[ixs[n_train:]]
 
-        if mode == 'train':
+        if mode == "train":
             self.x = normal_data[ixs[:n_train]]
             self.y = normal_labels[ixs[:n_train]]
-        elif mode == 'test':
-            anomalous_data = features[labels==1]
-            anomalous_labels = labels[labels==1]
+        elif mode == "test":
+            anomalous_data = features[labels == 1]
+            anomalous_labels = labels[labels == 1]
             self.x = jnp.concatenate((anomalous_data, normal_data_test), axis=0)
             self.y = jnp.concatenate((anomalous_labels, normal_labels_test), axis=0)
 
@@ -33,15 +35,18 @@ class KDDCupData:
 
     def __getitem__(self, index):
         """Return a sample from the dataset."""
-        return jnp.float32(self.x[index]), jnp.float32(self.y[index])
+        return np.float32(self.x[index]), np.float32(self.y[index])
 
-def get_dataloaders(batch_size, data_dir='./data/kddcup.npz'):
+
+def load_kddcup99(key, batch_size, data_dir="./data/kddcup.npz"):
     """Returning train and test dataloaders."""
-    train = KDDCupData(data_dir, 'train')
-    dataloader_train = DataLoader(train, batch_size=batch_size, 
-                              shuffle=True, num_workers=0)
-    
-    test = KDDCupData(data_dir, 'test')
-    dataloader_test = DataLoader(test, batch_size=batch_size, 
-                              shuffle=False, num_workers=0)
+    train = KDDCupData(key, data_dir, "train")
+    dataloader_train = DataLoader(
+        train, batch_size=batch_size, shuffle=True, num_workers=0
+    )
+
+    test = KDDCupData(key, data_dir, "test")
+    dataloader_test = DataLoader(
+        test, batch_size=batch_size, shuffle=False, num_workers=0
+    )
     return dataloader_train, dataloader_test
--- model.py
+++ model.py
@@ -1,15 +1,27 @@
 import jax.numpy as jnp
 from flax import nnx
+from jax.scipy.linalg import inv, cholesky
 
 import utils
 
+
 class DAGMM(nnx.Module):
-    # this is the exact model architecture the paper authors 
-    # used for evaluation on the KDDCUP99 dataset
-    def __init__(self, *, rngs: nnx.Rngs):
-        self.k = 4 
+    def __init__(
+        self,
+        n_features=122,
+        n_components=4,
+        lambda_1=0.1,
+        lambda_2=0.005,
+        *,
+        rngs: nnx.Rngs
+    ):
+        self.N = n_features
+        self.k = n_components
+        self.lambda_1 = lambda_1
+        self.lambda_2 = lambda_2
+
         # encoder network
-        self.cn1 = nnx.Linear(120, 60, rngs=rngs)
+        self.cn1 = nnx.Linear(self.N, 60, rngs=rngs)
         self.cn2 = nnx.Linear(60, 30, rngs=rngs)
         self.cn3 = nnx.Linear(30, 10, rngs=rngs)
         self.cn4 = nnx.Linear(10, 1, rngs=rngs)
@@ -17,37 +29,91 @@ class DAGMM(nnx.Module):
         self.cn5 = nnx.Linear(1, 10, rngs=rngs)
         self.cn6 = nnx.Linear(10, 30, rngs=rngs)
         self.cn7 = nnx.Linear(30, 60, rngs=rngs)
-        self.cn8 = nnx.Linear(60, 120, rngs=rngs)
+        self.cn8 = nnx.Linear(60, self.N, rngs=rngs)
         # estimation network
         self.en1 = nnx.Linear(3, 10, rngs=rngs)
         self.en2 = nnx.Dropout(0.5, rngs=rngs)
-        self.en3 = nnx.Linear(10, 4, rngs=rngs)
-    
+        self.en3 = nnx.Linear(10, self.k, rngs=rngs)
+
     def __encode(self, x):
         x = nnx.tanh(self.cn1(x))
         x = nnx.tanh(self.cn2(x))
         x = nnx.tanh(self.cn3(x))
         x = self.cn4(x)
         return x
-    
+
     def __decode(self, x):
         x = nnx.tanh(self.cn5(x))
         x = nnx.tanh(self.cn6(x))
         x = nnx.tanh(self.cn7(x))
         x = self.cn8(x)
         return x
-    
+
     def __estimate(self, x):
         x = nnx.tanh(self.en1(x))
         x = self.en2(x)
+        x = self.en3(x)
         x = nnx.softmax(x)
         return x
-    
+
     def __call__(self, x):
         z_c = self.__encode(x)
         x_hat = self.__decode(z_c)
-        z_r_1 = utils.euclidean_distance(x, x_hat)
-        z_r_2 = utils.cosine_similarity(x, x_hat)
-        z = jnp.concat(jnp.atleast_1d(z_r_1), jnp.atleast_1d(z_r_2), z_c)
+        z_r_1 = utils.euclidean_distance(x, x_hat).reshape(-1, 1)
+        # print(f'z_r_1.shape: {z_r_1.shape}')
+        # print(f'z_r_1: {z_r_1}')
+        z_r_2 = utils.cosine_similarity(x, x_hat).reshape(-1, 1)
+        # print(f'z_r_2.shape: {z_r_2.shape}')
+        # print(f'z_r_2: {z_r_2}')
+        z = jnp.concat((z_r_1, z_r_2, z_c), axis=1)
+        # print(f'z.shape: {z.shape}')
+        # print(f'z: {z}')
         gamma = self.__estimate(z)
-        return gamma, x_hat, z_c
+        return gamma, x_hat, z
+
+    @staticmethod
+    def calc_mixture_stats(inputs, gamma, z):
+        # gamma.shape: (1024, 4); x_hat.shape: (1024, 122); z.shape: (1024, 3)
+        # phi: same shape as gamma
+        gamma_sum = jnp.sum(gamma, axis=0)
+        phi = gamma_sum / inputs.shape[0]
+        # mu: (4, 3)
+        gamma_expanded = jnp.expand_dims(gamma, -1)  # Shape (1024, 4, 1)
+        z_expanded = jnp.expand_dims(z, 1)  # Shape (1024, 1, 3)
+        mu = jnp.sum(gamma_expanded * z_expanded, axis=0)
+        mu /= jnp.expand_dims(gamma_sum, -1)
+        # covariances: (4, 3, 3)
+        z_mu = z_expanded - jnp.expand_dims(mu, 0)  # Shape (1024, 4, 3)
+        z_mu_z_mu_t = jnp.expand_dims(z_mu, -1) * jnp.expand_dims(z_mu, -2)
+        covariances = jnp.sum(jnp.expand_dims(gamma_expanded, -1) * z_mu_z_mu_t, axis=0)
+        covariances /= jnp.expand_dims(jnp.expand_dims(gamma_sum, -1), -1)
+        return phi, mu, covariances
+
+    def calc_sample_energy(self, z, phi, mu, covs):
+        # broadcast differences
+        z_mu = jnp.expand_dims(z, 1) - jnp.expand_dims(mu, 0)  # Shape: (N, K, D)
+
+        eps = 1e-12
+        cov_invs = []
+        cov_dets = []
+
+        # Loop over components and calculate the covariance inverse and determinant
+        for i in range(self.k):
+            cov_i = covs[i] + (jnp.eye(covs[i].shape[-1]) * eps)  # numerical stability
+            cov_inv_i = inv(cov_i)
+            cov_invs.append(jnp.expand_dims(cov_inv_i, 0))  # Shape: (1, D, D)
+
+            L = cholesky(cov_i * (2 * jnp.pi), lower=True)
+            cov_det_i = jnp.prod(jnp.diagonal(L)) ** 2
+            cov_dets.append(jnp.expand_dims(cov_det_i, 0))  # Shape: (1,)
+
+        cov_invs = jnp.concatenate(cov_invs, axis=0)  # Shape: (K, D, D)
+        cov_dets = jnp.expand_dims(
+            jnp.sqrt(jnp.concatenate(cov_dets)), 0
+        )  # Shape: (K, 1)
+
+        E_z = jnp.sum(jnp.expand_dims(z_mu, -1) * jnp.expand_dims(cov_invs, 0), axis=-2)
+        E_z = jnp.sum(E_z * z_mu, axis=-1)
+        E_z = jnp.exp(-0.5 * E_z)
+        E_z = -jnp.log(jnp.sum(jnp.expand_dims(phi, 0) * E_z / cov_dets, axis=1) + eps)
+        return jnp.mean(E_z)
